7568f9a500753a6a2a372af84313e224940e3099
Jonathan <jonathan.riedmair@pm.me>
2025-02-16 18:51:49 +0100

    Update auto_clear_metadata.sh

--- abs/batch-m4b-convert/scripts/auto_clear_metadata.sh
+++ abs/batch-m4b-convert/scripts/auto_clear_metadata.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # Clear ABS-metadata (automatically and safe - only deletes mp3-files if no FFMPEG-process is running!)
 
-FFMPEG_PROCS=$(ps -ef | grep ffmpeg | wc -l)
-
 # Output for the logfile
 current_date_time="`date "+%Y-%m-%d %H:%M:%S"`";
 echo -en "\n------------------------------------------------------------------\n$current_date_time\n"
 
-
+FFMPEG_PROCS=$(ps -ef | grep ffmpeg | wc -l)
 if [ "$FFMPEG_PROCS" -lt "2" ]; then
     echo -e "\e[92mStart automatic metadata-cleanup:\e[0m"
     sudo find /usr/share/audiobookshelf/metadata/cache/items/ -type f -iname *.mp3 -delete
